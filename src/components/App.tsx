f3eaa7afe9ce5806322d673501b3c285ae463317
Viktoria T <deviastery@gmail.com>
2024-03-22 00:14:12 +0300

    feature(App): add output of all posts with a delay

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,50 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
 import { getApiResource } from '../utils/getPostsData'
 
 import { IPost } from "../types/data";
 
 export const App: React.FC = () => {
-    const [posts, setPosts] = useState<IPost[]>([])
-    const [countPosts, setCountPosts] = useState(0)
+    const [posts, setPosts] = useState<IPost[]>([]);
+    const [nextCountPosts, setNextCountPosts] = useState(10);
 
     const getResourse = async (url : string) : Promise<IPost[] | boolean> => {
         const res = await getApiResource(url);
 
-        if (Array.isArray(res)) {
-            const nextPosts = res.slice(0, 10);
-            setCountPosts(countPosts + 10);
-
-            setPosts(nextPosts);
-        }
-
         return res;
-    }
+    };
+
+    const handleScroll = () => {
+        if (
+            window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+            posts.length > 0 &&
+            nextCountPosts <= posts.length
+        ) {
+            setNextCountPosts(nextCountPosts + 10);
+            console.log(nextCountPosts+10);
+        }
+    };
 
     useEffect(() => {
-        getResourse('https://jsonplaceholder.typicode.com/posts');
-    }, [])
+        (async () => {
+            const res = await getResourse('https://jsonplaceholder.typicode.com/posts');
+
+            console.log(nextCountPosts);
+            if (Array.isArray(res)) {
+
+                    console.log(res);
+                    setPosts(res);
+                }
+        })();
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        
+    }, [posts, nextCountPosts]);
 
     return (
         <div>
-            {posts.map((post: IPost, index: number) => (
+            {posts.slice(0, nextCountPosts).map((post: IPost, index: number) => (
                 <div key={index}>
                     <h3>{post.title}</h3>
                     <p>{post.body}</p>
